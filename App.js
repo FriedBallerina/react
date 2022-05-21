12a07cae81246f02275b24b493a5e54f414e85de
FriedBallerina <FriedBallerina@users.noreply.github.com>
2022-05-21 15:10:26 +0300

    added Comment class, class object

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import { useState } from "react";
 
+class Comment {
+  constructor(id, content) {
+    this.id = id;
+    this.content = content;
+  }
+}
+
 const Display = ({ counter }) => (
   <div>Number of comments so far: {counter}</div>
 );
 
 const App = () => {
-  let arr = ["hello "];
-  const [comments, setComments] = useState(arr);
-  const addComment = () =>
-    setComments((comments) => comments.concat("comment nr" + comments.length));
-
+  const [comments, setComments] = useState([]);
+  const p = new Comment(comments.length + 1, "new comment");
+  const addComment = () => {
+    setComments((comments) => comments.concat(p));
+  };
   return (
     <>
-      <ul>{comments.join(" ")}</ul>
-      <Display counter={comments.length - 1} />
+      <h1>First post</h1>
+      <ul>
+        {comments.map((p) => (
+          <li key={p.id}>{p.content + " " + p.id}</li>
+        ))}
+      </ul>
+      <Display counter={comments.length} />
       <button onClick={addComment}>Add a comment</button>
     </>
   );
