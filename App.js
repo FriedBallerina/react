665eff3538ea8c1c9e70bbf756a30835c855b3c7
FriedBallerina <FriedBallerina@users.noreply.github.com>
2022-05-21 14:25:11 +0300

    refactored the component

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { useState } from "react";
 
 const App = () => {
-  const Display = (props) => {
-    return <div>Number of comments so far: {props.counter}</div>;
-  };
+  const Display = ({ counter }) => (
+    <div>Number of comments so far: {counter}</div>
+  );
 
   let arr = ["hello "];
   const [comments, setComments] = useState(arr);
-  const addComment = () => {
+  const addComment = () =>
     setComments((comments) => [...comments, " comment nr" + comments.length]);
-  };
 
   return (
     <>
       <ul>{comments}</ul>
-      <Display counter={comments.length} />
+      <Display counter={comments.length - 1} />
       <button onClick={addComment}>Add a comment</button>
     </>
   );
