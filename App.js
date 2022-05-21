7f55a04d200638afb2060c660f26c86b8a48c3ef
FriedBallerina <FriedBallerina@users.noreply.github.com>
2022-05-21 14:41:55 +0300

    .concat() and .join()

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import { useState } from "react";
 
-const App = () => {
-  const Display = ({ counter }) => (
-    <div>Number of comments so far: {counter}</div>
-  );
+const Display = ({ counter }) => (
+  <div>Number of comments so far: {counter}</div>
+);
 
+const App = () => {
   let arr = ["hello "];
   const [comments, setComments] = useState(arr);
   const addComment = () =>
-    setComments((comments) => [...comments, " comment nr" + comments.length]);
+    setComments((comments) => comments.concat("comment nr" + comments.length));
 
   return (
     <>
-      <ul>{comments}</ul>
+      <ul>{comments.join(" ")}</ul>
       <Display counter={comments.length - 1} />
       <button onClick={addComment}>Add a comment</button>
     </>
