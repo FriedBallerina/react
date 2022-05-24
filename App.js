7e6a6ab12529bce2dffec5ff2dc72260abc92a80
FriedBallerina <FriedBallerina@users.noreply.github.com>
2022-05-24 19:01:10 +0300

    axios, promise, useEffect

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Display = ({ counter }) => (
   <div>
@@ -8,7 +9,6 @@ const Display = ({ counter }) => (
 );
 
 const AllComments = ({ stuff }) => {
-  console.log(stuff);
   return (
     <>
       {stuff.map((r) => (
@@ -21,34 +21,22 @@ const AllComments = ({ stuff }) => {
 
 const App = () => {
   const [comments, setComments] = useState([]);
-  const [newComment, setNewComment] = useState("");
 
-  const addComment = (event) => {
-    event.preventDefault();
-    const commentObject = {
-      content: newComment,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: comments.length + 1,
-    };
-    setComments((comments) => comments.concat(commentObject));
-    setNewComment("");
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3002/notes").then((response) => {
+      console.log("promise fulfilled");
+      setComments(response.data);
+      console.log(response.data);
+    });
   };
 
-  const handleCommentChange = (event) => {
-    console.log(event.target.value);
-    setNewComment(event.target.value);
-  };
+  useEffect(hook, []);
+  console.log("render", comments.length, "comments");
 
   return (
     <>
       <h1>A post</h1>
-      <div></div>
-      <p>Some random content. The comments are below.</p>
-      <form onSubmit={addComment}>
-        <input value={newComment} onChange={handleCommentChange} />
-        <button type="submit">Add a comment</button>
-      </form>
       <Display counter={comments.length} />
       <div>
         <p>Comments:</p>
