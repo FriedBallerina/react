ac9cad1cf35d31102dfec6c281e5942116f963a3
FriedBallerina <FriedBallerina@users.noreply.github.com>
2022-06-03 14:02:38 +0300

    post to json db

--- App.js
+++ App.js
@@ -1,48 +1,59 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
-const Display = ({ counter }) => (
-  <div>
-    <br />
-    Number of comments so far: {counter}
-  </div>
-);
-
-const AllComments = ({ stuff }) => {
-  return (
-    <>
-      {stuff.map((r) => (
-        <li key={r.id}>{r.content}</li>
-      ))}
-      <br />
-    </>
-  );
-};
+import Note from "./components/Note";
 
 const App = () => {
-  const [comments, setComments] = useState([]);
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
-    console.log("effect");
+  useEffect(() => {
     axios.get("http://localhost:3002/notes").then((response) => {
-      console.log("promise fulfilled");
-      setComments(response.data);
-      console.log(response.data);
+      setNotes(response.data);
     });
+  }, []);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    };
+
+    axios.post("http://localhost:3002/notes", noteObject).then((response) => {
+      console.log(response);
+    });
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
   };
 
-  useEffect(hook, []);
-  console.log("render", comments.length, "comments");
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   return (
-    <>
-      <h1>A post</h1>
-      <Display counter={comments.length} />
+    <div>
+      <h1>Notes</h1>
       <div>
-        <p>Comments:</p>
-        <AllComments stuff={comments} />
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
       </div>
-    </>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
   );
 };
 
